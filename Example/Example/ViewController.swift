3a5bec037433ec6836a867f3d2e7f712cb0d14a3
Nadya Karaban <nadya@gini.net>
2021-03-26 19:03:17 +0100

    * project setup;   * installed pods;   * added example target (PIA-1340)

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Nadya Karaban on 26.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
