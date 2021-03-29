c1d0e5c7701c5368d17b9f38304b037d1b0e0697
Nadya Karaban <nadya@gini.net>
2021-03-29 18:35:03 +0200

    added tests target (PIA-1340)

--- Example/GiniPayBusinessSDKTests/GiniPayBusinessSDKTests.swift
+++ Example/GiniPayBusinessSDKTests/GiniPayBusinessSDKTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import GiniPayBusinessSDK
+
 
 class GiniPayBusinessSDKTests: XCTestCase {
 
---
+++ GiniPayBusiness/Classes/Core/GiniPayBank.swift
@@ -0,0 +1,8 @@
+//
+//  GiniPayBank.swift
+//  GiniPayBank
+//
+//  Created by Nadya Karaban on 18.02.21.
+//
+
+import Foundation
