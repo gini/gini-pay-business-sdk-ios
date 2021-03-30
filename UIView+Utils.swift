7bbcdbf8a6862db6445b33d3c43676d794864c5e
Nadya Karaban <nadya@gini.net>
2021-03-30 17:36:13 +0200

    * PaymentReview : added payment fields (PIA-931)   * Added Util and extension for UIButton and UIView customisation (PIA-931)

---
+++ UIView+Utils.swift
@@ -0,0 +1,39 @@
+//
+//  UIView+Utils.swift
+//  GiniPayBusiness
+//
+//  Created by Nadya Karaban on 30.03.21.
+//
+
+import Foundation
+@IBDesignable class GiniView: UIView {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
