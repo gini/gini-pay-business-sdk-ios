4d37fe10f11419f47f3653cb12e24b04bb0d3878
Nadya Karaban <nadya@gini.net>
2021-03-30 18:19:06 +0200

    * moved PaymentPreview.storyboard and Utils files in the core directory;   * added GiniPayBusinessConfiguration: as a configuration for UI customisation;   * added customFont aka GiniPayFont and custom color for dark mode support aka GiniColor (PIA-931)

---
+++ GiniPayBusiness/Classes/Core/GiniPayBusinessConfiguration.swift
@@ -0,0 +1,53 @@
+//
+//  GiniPayBusinessConfiguration.swift
+//  GiniPayBusiness
+//
+//  Created by Nadya Karaban on 30.03.21.
+//
+
+import Foundation
+public final class GiniPayBusinessConfiguration: NSObject {
+    
+    /**
+     Singleton to make configuration internally accessible in all classes of the Gini Pay Business SDK.
+     */
+    static var shared = GiniPayBusinessConfiguration()
+    
+    /**
+     Returns a `GiniPayBusinessConfiguration` instance which allows to set individual configurations
+     to change the look and feel of the Gini Pay Business SDK.
+     
+     - returns: Instance of `GiniPayBusinessConfiguration`.
+     */
+    public override init() {}
+    
+    
+    /**
+     Sets the backgroundColor  on the payment revierw screen for pay button
+     */
+    @objc public var payButtonBackgroundColor = GiniColor(lightModeColor: .blue, darkModeColor: .blue)
+    
+    /**
+     Sets the font of the pay button on the payment revierw screen
+     */
+    @objc public var payButtonTextFont = UIFont.systemFont(ofSize: 20, weight: .bold)
+    
+    /**
+     Sets the text color of the pay button on the payment revierw screen
+     */
+    @objc public var payButtonTextColor = GiniColor(lightModeColor: .white, darkModeColor: .black)
+    
+    /**
+     Sets the font used in the Return Assistant screens by default.
+     */
+    
+    @objc public lazy var customFont = GiniFont(regular: UIFont.systemFont(ofSize: 14,
+                                                                                                 weight: .regular),
+                                                                      bold: UIFont.systemFont(ofSize: 14,
+                                                                                              weight: .bold),
+                                                                      light: UIFont.systemFont(ofSize: 14,
+                                                                                               weight: .light),
+                                                                      thin: UIFont.systemFont(ofSize: 14,
+                                                                                              weight: .thin),
+                                                                      isEnabled: false)
+}
---
+++ GiniPayBusiness/Classes/Core/PaymentReviewViewController.swift
@@ -0,0 +1,11 @@
+//
+//  PaymentReviewViewController.swift
+//  GiniPayBusiness
+//
+//  Created by Nadya Karaban on 30.03.21.
+//
+
+import Foundation
+final class PaymentReviewViewController: UIViewController {
+    var giniPayBusinessConfiguration =  GiniPayBusinessConfiguration()
+}
