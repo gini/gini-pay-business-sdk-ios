22f886d4e4b6d859a1befc502777877cfdf6d03f
Nadya Karaban <nadya@gini.net>
2021-04-08 13:40:13 +0200

    added configurable properties for input text fields (PIA-931)

--- GiniPayBusiness/Classes/Core/GiniPayBusinessConfiguration.swift
+++ GiniPayBusiness/Classes/Core/GiniPayBusinessConfiguration.swift
@@ -25,7 +25,7 @@ public final class GiniPayBusinessConfiguration: NSObject {
     /**
      Sets the backgroundColor  on the payment review screen for pay button
      */
-    @objc public var payButtonBackgroundColor = GiniColor(lightModeColor: .blue, darkModeColor: .blue)
+    @objc public var payButtonBackgroundColor = GiniColor(lightModeColor: UIColor.from(hex:0xFF6800), darkModeColor: UIColor.from(hex:0xFF6800))
     
     /**
      Sets the font of the pay button on the payment review screen
@@ -35,15 +35,40 @@ public final class GiniPayBusinessConfiguration: NSObject {
     /**
      Sets the text color of the pay button on the payment review screen
      */
-    @objc public var payButtonTextColor = GiniColor(lightModeColor: .white, darkModeColor: .black)
+    @objc public var payButtonTextColor = GiniColor(lightModeColor: .white, darkModeColor: .white)
     
     /**
      Sets the corner radius of the pay button on the payment review screen
      */
-    @objc public var payButtonCornerRadius = 6.0
+    @objc public var payButtonCornerRadius: CGFloat = 6.0
     
     /**
-     Sets the font used in the Return Assistant screens by default.
+     Sets the font of the payment input fields on the payment review screen
+     */
+    @objc public var paymentInputFieldFont = UIFont.systemFont(ofSize: 17, weight: .semibold)
+    
+    /**
+     Sets the background color of the payment input fields on the payment review screen
+     */
+    @objc public var paymentInputFieldBackgroundColor = GiniColor(lightModeColor: UIColor.from(hex: 0xF2F3F6), darkModeColor: UIColor.from(hex: 0xF2F3F6))
+    
+    /**
+     Sets the text color of the payment input fields on the payment review screen
+     */
+    @objc public var paymentInputFieldTextColor = GiniColor(lightModeColor: UIColor.from(hex: 0x33406F), darkModeColor: UIColor.from(hex: 0x33406F))
+
+    /**
+     Sets the placeholder font of the payment input fields on the payment review screen
+     */
+    @objc public var paymentInputFieldPlaceholderFont = UIFont.systemFont(ofSize: 17, weight: .light)
+    
+    /**
+     Sets the placeholder text color of the payment input fields on the payment review screen
+     */
+    @objc public var paymentInputFieldPlaceholderTextColor = GiniColor(lightModeColor: UIColor.from(hex: 0x999FB7), darkModeColor: UIColor.from(hex: 0x999FB7))
+    
+    /**
+     Sets the font used in the screens by default.
      */
     
     @objc public lazy var customFont = GiniFont(regular: UIFont.systemFont(ofSize: 14,
