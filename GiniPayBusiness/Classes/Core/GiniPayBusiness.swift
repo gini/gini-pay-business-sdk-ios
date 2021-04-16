996f35ef12fd1712c3dfc08665964a70ab90b61c
Nadya Karaban <nadya@gini.net>
2021-04-16 16:07:49 +0200

    GiniPayBusiness:   * init(with giniApiLib: GiniApiLib)   * setConfiguration(_ configuration: GiniPayBusinessConfiguration)   * check if any payment providers is available (PIA-1341)

--- GiniPayBusiness/Classes/Core/GiniPayBusiness.swift
+++ GiniPayBusiness/Classes/Core/GiniPayBusiness.swift
@@ -6,6 +6,65 @@
 //
 
 import Foundation
+import GiniPayApiLib
 
-@objc public final class GiniPayBusiness: NSObject {}
+public enum GiniPayBusinessError: Error {
+    case noInstalledApps
+    case apiError(GiniError)
+}
+
+@objc public final class GiniPayBusiness: NSObject {
+    
+    public var giniApiLib: GiniApiLib
+    public var documentService: DefaultDocumentService
+    public var paymentService: PaymentService
+    private var bankProviders: [PaymentProvider] = []
+    
+    public init(with giniApiLib: GiniApiLib){
+        self.giniApiLib = giniApiLib
+        self.documentService = giniApiLib.documentService()
+        self.paymentService = giniApiLib.paymentService()
+    }
+    
+    private func getInstalledBankingApps() -> [PaymentProvider] {
+        self.paymentService.paymentProviders { result in
+            switch result {
+            case let .success(providers):
+                for provider in providers {
+                    DispatchQueue.main.async {
+                        if let url = URL(string: provider.appSchemeIOS) {
+                            if UIApplication.shared.canOpenURL(url) {
+                                self.bankProviders.append(provider)
+                            }
+                        }
+                    }
+                }
+            case let .failure(error):
+                print(error.localizedDescription)
+            }
+        }
+        return bankProviders
+    }
+
+    /**
+     Checks if there are any banking app which support Gini Pay fuctionaly installed
+     for example to change texts and colors displayed to the user.
+     
+     */
+    public func checkIfAnyPaymentProviderAvailiable() -> Bool {
+        let installedApps = getInstalledBankingApps()
+        return installedApps.count > 0
+    }
+    
+    /**
+     Sets a configuration which is used to customize the look of the Gini Pay Business SDK,
+     for example to change texts and colors displayed to the user.
+     
+     - parameter configuration: The configuration to set.
+     */
+    public func setConfiguration(_ configuration: GiniPayBusinessConfiguration) {
+        GiniPayBusinessConfiguration.shared = configuration
+    }
+    
+}
 
