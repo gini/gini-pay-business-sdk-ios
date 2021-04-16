103e1b91f291ff4fc1c4d0393ee55d3428952a32
Nadya Karaban <nadya@gini.net>
2021-04-16 16:39:23 +0200

    Business app:   * added CredentialsManager;   * added extension for standard error view ;   * added check if any bank app is installed (PIA-1341)

--- Example/Example Swift/CameraViewController.swift
+++ Example/Example Swift/CameraViewController.swift
@@ -5,28 +5,55 @@
 //  Created by Nadya Karaban on 15.04.21.
 //
 
-import UIKit
+import GiniPayApiLib
+import GiniPayBusiness
 import Photos
+import UIKit
+
 class CameraViewController: UIViewController {
-
     @IBOutlet var imageView: UIImageView!
-
+    var apiLib: GiniApiLib!
     var imagePicker: ImagePicker!
+    var businessSDK: GiniPayBusiness!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+        imagePicker = ImagePicker(presentationController: self, delegate: self)
+        initializeSDK()
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if !businessSDK.checkIfAnyPaymentProviderAvailiable(){
+            showError("", message: "Unfortunately, we didn't find any app which support Gini Pay")
+        }
     }
 
     @IBAction func showImagePicker(_ sender: UIButton) {
-        self.imagePicker.present(from: sender)
+        imagePicker.present(from: sender)
+    }
+
+    func initializeSDK() {
+        let client = CredentialsManager.fetchClientFromBundle()
+        apiLib = GiniApiLib.Builder(client: client).build()
+        businessSDK = GiniPayBusiness(with: apiLib)
     }
 }
 
 extension CameraViewController: ImagePickerDelegate {
-
     func didSelect(image: UIImage?) {
-        self.imageView.image = image
+        imageView.image = image
+    }
+}
+
+
+extension CameraViewController {
+    func showError(_ title: String, message: String) {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(OKAction)
+        present(alertController, animated: true, completion: nil)
     }
 }
