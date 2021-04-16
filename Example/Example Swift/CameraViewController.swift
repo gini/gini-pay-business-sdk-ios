be7edb14345da782534248e2eb892ae2a8f885b7
Nadya Karaban <nadya@gini.net>
2021-04-16 20:45:14 +0200

    Business example app:   * added upload image with partial and composite document creation;   * added precondition check for installed banking apps (PIA-1341)

--- Example/Example Swift/CameraViewController.swift
+++ Example/Example Swift/CameraViewController.swift
@@ -15,17 +15,25 @@ class CameraViewController: UIViewController {
     var apiLib: GiniApiLib!
     var imagePicker: ImagePicker!
     var businessSDK: GiniPayBusiness!
+    var docId = ""
+    var paymentProviders: PaymentProviders = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker = ImagePicker(presentationController: self, delegate: self)
         initializeSDK()
-        
     }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        if !businessSDK.checkIfAnyPaymentProviderAvailiable(){
-            showError("", message: "Unfortunately, we didn't find any app which support Gini Pay")
+        businessSDK.checkIfAnyPaymentProviderAvailiable {result in
+            switch result {
+            case let .success(providers):
+                self.paymentProviders = providers
+            case let .failure(error):
+                self.showError(message: error.localizedDescription)
+            }
         }
     }
 
@@ -38,22 +46,57 @@ class CameraViewController: UIViewController {
         apiLib = GiniApiLib.Builder(client: client).build()
         businessSDK = GiniPayBusiness(with: apiLib)
     }
+
+    fileprivate func createCompositeDocument(createdDocument: Document, completion: @escaping (Result<String, GiniPayBusinessError>) -> Void) {
+        let partialDocs = [PartialDocumentInfo(document: createdDocument.links.document)]
+        self.businessSDK.documentService
+            .createDocument(fileName: "ginipay-composite",
+                            docType: nil,
+                            type: .composite(CompositeDocumentInfo(partialDocuments: partialDocs)),
+                            metadata: nil) { [weak self] result in
+                guard self != nil else { return }
+                switch result {
+                case let .success(createdDocument):
+                    completion(.success(createdDocument.id))
+                    
+                case let .failure(error):
+                    completion(.failure(.apiError(error)))
+                }
+            }
+    }
+    
+    func uploadDocument(documentData: Data, completion: @escaping (Result<String, GiniPayBusinessError>) -> Void) {
+        businessSDK.documentService.createDocument(fileName: "ginipay-partial",
+                                                   docType: nil,
+                                                   type: .partial(documentData),
+                                                   metadata: nil) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(createdDocument):
+                DispatchQueue.main.async {
+                    self.createCompositeDocument(createdDocument: createdDocument, completion: completion)
+                }
+            case let .failure(error):
+                DispatchQueue.main.async {
+                    completion(.failure(.apiError(error)))
+                }
+            }
+        }
+    }
 }
 
 extension CameraViewController: ImagePickerDelegate {
     func didSelect(image: UIImage?) {
         imageView.image = image
-    }
-}
-
-
-extension CameraViewController {
-    func showError(_ title: String, message: String) {
-        let alertController = UIAlertController(title: title,
-                                                message: message,
-                                                preferredStyle: .alert)
-        let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alertController.addAction(OKAction)
-        present(alertController, animated: true, completion: nil)
+        guard let data = image?.jpegData(compressionQuality: 0.2) else { return }
+        self.uploadDocument(documentData: data) { result in
+            switch result {
+            case .success(let documentId):
+                self.docId = documentId
+                print(String(documentId) + "upload")
+            case .failure(let error):
+                self.showError(message: error.localizedDescription)
+            }
+        }
     }
 }
