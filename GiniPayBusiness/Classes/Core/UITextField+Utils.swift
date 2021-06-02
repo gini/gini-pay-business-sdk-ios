7e5766a7cddf83b29d07dbea0c60d207dde5f009
Alpar Szotyori <alpar@gini.net>
2021-06-02 09:49:06 +0200

    Add `UITextField` extension for moving the cursor position

--- GiniPayBusiness/Classes/Core/UITextField+Utils.swift
+++ GiniPayBusiness/Classes/Core/UITextField+Utils.swift
@@ -10,4 +10,11 @@ public extension UITextField {
     var isReallyEmpty: Bool {
         return text?.trimmingCharacters(in: .whitespaces).isEmpty ?? true
     }
+    
+    func moveSelectedTextRange(from position: UITextPosition, to offset: Int) {
+        if let newSelectedRangeFromTo = self.position(from: position, offset: offset),
+           let newSelectedRange = self.textRange(from: newSelectedRangeFromTo, to: newSelectedRangeFromTo) {
+            self.selectedTextRange = newSelectedRange
+        }
+    }
 }
